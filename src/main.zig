233228daf742d4c2f3c1660fe69df5d0ed1d5d00
umut <umut@sent.com>
2026-05-24 23:03:41 +0300

    use i32 for to use less cast

--- src/main.zig
+++ src/main.zig
@@ -17,23 +17,22 @@ pub fn main() !void {
 const GameState = enum { Idle, Play, Dead };
 
 const Game = struct {
-    sw: f32 = 1280,
-    sh: f32 = 720,
+    sw: i32 = 1280,
+    sh: i32 = 720,
     dt: f32 = 0,
 
-    cell_size: u16 = 10,
-    columns: u16 = 128,
-    rows: u16 = 72,
+    cell_size: i32 = 10,
+    columns: i32 = 128,
+    rows: i32 = 72,
 
     grid: [128][72]bool = [_][72]bool{[_]bool{false} ** 72} ** 128,
-    cursor_x: u16 = 0,
-    cursor_y: u16 = 0,
+    cursor_x: i32 = 0,
+    cursor_y: i32 = 0,
     create_cell_action: bool = false,
 
     pub fn update(game: *Game) void {
-        const cell_size: i32 = @intCast(game.cell_size);
-        var cursor_x: i32 = @divTrunc(rl.getMouseX(), cell_size);
-        var cursor_y: i32 = @divTrunc(rl.getMouseY(), cell_size);
+        var cursor_x: i32 = @divTrunc(rl.getMouseX(), game.cell_size);
+        var cursor_y: i32 = @divTrunc(rl.getMouseY(), game.cell_size);
 
         if (cursor_x >= 128) {
             cursor_x = 127;
@@ -59,7 +58,7 @@ const Game = struct {
 
         rl.clearBackground(.ray_white);
 
-        const current = &game.grid[game.cursor_x][game.cursor_y];
+        const current = &game.grid[@intCast(game.cursor_x)][@intCast(game.cursor_y)];
 
         //create new cell if clicked
         if (game.create_cell_action) {
@@ -69,24 +68,22 @@ const Game = struct {
         }
 
         //Draw Grid
-        for (0..game.columns) |x| {
+        for (0..@intCast(game.columns)) |x| {
             const pos_x: i32 = @intCast(x);
-            const pos_end: i32 = @intFromFloat(game.sh);
             rl.drawLine(
                 pos_x * game.cell_size,
                 0,
                 pos_x * game.cell_size,
-                pos_end,
+                game.sh,
                 .light_gray,
             );
         }
-        for (0..game.rows) |y| {
+        for (0..@intCast(game.rows)) |y| {
             const pos_y: i32 = @intCast(y);
-            const pos_end: i32 = @intFromFloat(game.sw);
             rl.drawLine(
                 0,
                 pos_y * game.cell_size,
-                pos_end,
+                game.sw,
                 pos_y * game.cell_size,
                 .light_gray,
             );
@@ -104,12 +101,11 @@ const Game = struct {
         }
 
         //Draw cells
-        for (0..game.columns) |x| {
-            for (0..game.rows) |y| {
+        for (0..@intCast(game.columns)) |x| {
+            for (0..@intCast(game.rows)) |y| {
                 if (game.grid[x][y]) {
                     const px: i32 = @intCast(x);
                     const py: i32 = @intCast(y);
-
                     rl.drawRectangle(
                         px * game.cell_size,
                         py * game.cell_size,
